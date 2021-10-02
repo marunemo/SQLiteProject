36705f38955c3eb0bb08c186ef24a13fc78bfa6e
marunemo <chys3697@gmail.com>
2021-10-03 00:09:33 +0900

    fixed main format mistake

--- src/mainProjcet/Main.java
+++ src/mainProjcet/Main.java
@@ -3,7 +3,7 @@ package mainProjcet;
 import java.util.Scanner;
 
 public class Main {
-	public static void main() {
+	public static void main(String[] args) {
 		try {
 			String dbName = "mySQLite.db";
 			String tableName = "todolist";
